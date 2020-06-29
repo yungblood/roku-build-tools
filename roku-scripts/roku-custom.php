55a60b73d78b7d84609618b151ed3f3d1b5c2aaa
Kevin Hoos <kevin.hoos@cbsinteractive.com>
2020-06-29 14:27:35 -0700

    Updates to improve jenkins

--- roku-scripts/roku-custom.php
+++ roku-scripts/roku-custom.php
@@ -196,10 +196,23 @@ function build() {
     install();
 }
 
+function read_jenkinsfile() {
+    if(!is_file("Jenkinsfile")) finish("Unable to find: Jenkinsfile", -1);
+    $jenkins = file_get_contents("Jenkinsfile");
+    $envMapStart = strpos($jenkins, "[");
+    $envMapLen = strpos($jenkins, "//end environmentMap") - $envMapStart;
+    //Convert to clean json.
+    $json = str_replace(["[","]","\n","\t"],["{","}","",""], substr($jenkins, $envMapStart, $envMapLen));
+    while (strpos($json, " ") !== false) $json = str_replace(" ", "", $json);
+    $json = str_replace(",}", "}", $json);
+    $envMap = json_decode($json, true);
+    return $envMap;
+}
+
 function jenkins_pkg() {
     global $E;
     if(!is_file("Jenkinsfile")) finish("Unable to find: Jenkinsfile", -1);
-    if(!isset($E['JENKINS']))  finish("Required var not set: JENKINS", -1);
+    if(!isset($E['JENKINS'])) finish("Required var not set: JENKINS", -1);
     $jenkins = file_get_contents("Jenkinsfile");
     $envMapStart = strpos($jenkins, "[");
     $envMapLen = strpos($jenkins, "//end environmentMap") - $envMapStart;
@@ -221,5 +234,31 @@ function jenkins_pkg() {
     updateEnv();
     all();
     curl_upload_artifactory();
+}
+
+function jenkins() {
+    global $E;
+    if(empty($E['COUNTRY'])) finish("Required var not set: COUNTRY", -1);
+    if(empty($E['BUILD_TYPE'])) finish("Required var not set: BUILD_TYPE", -1);
+    $envMap = read_jenkinsfile();
+    if(!empty($envMap[$E['COUNTRY']])) $country = $E['COUNTRY'];
+    if(!isset($country)) finish("Unknown region: $E[COUNTRY]", -1);
+    foreach ($envMap[$country]["default"] as $key=>$val) {
+        if(empty($E[$key])) $E[$key] = $val;
+    }
+    foreach ($envMap[$country] as $key=>$val) {
+        if(!empty($val['BUILD_TYPE']) and $val['BUILD_TYPE'] === $E['BUILD_TYPE']) {
+            $build = $key;
+            foreach ($envMap[$country][$build] as $key=>$val) {
+                $E[$key] = $val;
+            }
+            updateEnv();
+            all();
+            curl_upload_artifactory();
+            if(empty($E['ARTIFACTORY_LIST'])) $E['ARTIFACTORY_LIST'] = "";
+            if(file_exists("$E[ZIPDIR]/$E[APPFULLNAME].zip")) $E['ARTIFACTORY_LIST'] .= sprintf("\n $E[ARTIFACTORY]/$E[APPFULLNAME].zip ");
+            if(file_exists("$E[PKGDIR]/$E[APPFULLNAME].pkg")) $E['ARTIFACTORY_LIST'] .= sprintf("\n $E[ARTIFACTORY]/$E[APPFULLNAME].pkg ");
+        }
+    }
 }
 ?>
